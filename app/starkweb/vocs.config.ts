f6085e43b7a3aed11ba231bd93fd16c5515964a8
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2024-12-19 11:10:49 +0100

    local change

--- app/starkweb/vocs.config.ts
+++ app/starkweb/vocs.config.ts
@@ -36,72 +36,7 @@ export default defineConfig({
         },
       ],
     },
-    {
-      text: "React",
-      items: [
-        {
-          text: "Getting Started",
-          link: "/react/",
-        },
-        {
-          text: "Configuration",
-          items: [
-            {
-              text: "createConfig",
-              link: "/react/api/createConfig",
-            },
-            {
-              text: "createStorage",
-              link: "/react/api/createStorage",
-            },
-            {
-              text: "Chains",
-              link: "/react/api/Chains",
-            },
-            {
-              text: "StarkwebProvider",
-              link: "/react/api/StarkwebProvider",
-            },
-          ],
-        },
-        {
-          text: "Hooks",
-          link: "/react/hooks",
-          collapsed: true,
-          items: [
-            { text: "useAccount", link: "/react/hooks/useAccount" },
-            { text: "useAccountEffect", link: "/react/hooks/useAccountEffect" },
-            { text: "useBalance", link: "/react/hooks/useBalance" },
-            { text: "useBlockNumber", link: "/react/hooks/useBlockNumber" },
-            { text: "useChainId", link: "/react/hooks/useChainId" },
-            { text: "useClient", link: "/react/hooks/useClient" },
-            { text: "useConfig", link: "/react/hooks/useConfig" },
-            { text: "useConnect", link: "/react/hooks/useConnect" },
-            { text: "useConnections", link: "/react/hooks/useConnections" },
-            {
-              text: "useConnectorClient",
-              link: "/react/hooks/useConnectorClient",
-            },
-            { text: "useConnectors", link: "/react/hooks/useConnectors" },
-            { text: "useContractRead", link: "/react/hooks/useContractRead" },
-            { text: "useContractReads", link: "/react/hooks/useContractReads" },
-            { text: "useContractWrite", link: "/react/hooks/useContractWrite" },
-            { text: "useDisconnect", link: "/react/hooks/useDisconnect" },
-            { text: "useReconnect", link: "/react/hooks/useReconnect" },
-            { text: "useSignMessage", link: "/react/hooks/useSignMessage" },
-            { text: "useSignTypedData", link: "/react/hooks/useSignTypedData" },
-            { text: "useSwitchAccount", link: "/react/hooks/useSwitchAccount" },
-            { text: "useSwitchChain", link: "/react/hooks/useSwitchChain" },
-            { text: "useVerifyMessage", link: "/react/hooks/useVerifyMessage" },
-            { text: "useWalletClient", link: "/react/hooks/useWalletClient" },
-          ],
-        },
-      ],
-    },
-    // {
-    //   text: "Connect",
-    //   items: [],
-    // },
+
     {
       text: "Core",
       items: [
@@ -338,5 +273,71 @@ export default defineConfig({
         },
       ],
     },
+    {
+      text: "React",
+      items: [
+        {
+          text: "Getting Started",
+          link: "/react/",
+        },
+        {
+          text: "Configuration",
+          items: [
+            {
+              text: "createConfig",
+              link: "/react/api/createConfig",
+            },
+            {
+              text: "createStorage",
+              link: "/react/api/createStorage",
+            },
+            {
+              text: "Chains",
+              link: "/react/api/Chains",
+            },
+            {
+              text: "StarkwebProvider",
+              link: "/react/api/StarkwebProvider",
+            },
+          ],
+        },
+        {
+          text: "Hooks",
+          link: "/react/hooks",
+          collapsed: true,
+          items: [
+            { text: "useAccount", link: "/react/hooks/useAccount" },
+            { text: "useAccountEffect", link: "/react/hooks/useAccountEffect" },
+            { text: "useBalance", link: "/react/hooks/useBalance" },
+            { text: "useBlockNumber", link: "/react/hooks/useBlockNumber" },
+            { text: "useChainId", link: "/react/hooks/useChainId" },
+            { text: "useClient", link: "/react/hooks/useClient" },
+            { text: "useConfig", link: "/react/hooks/useConfig" },
+            { text: "useConnect", link: "/react/hooks/useConnect" },
+            { text: "useConnections", link: "/react/hooks/useConnections" },
+            {
+              text: "useConnectorClient",
+              link: "/react/hooks/useConnectorClient",
+            },
+            { text: "useConnectors", link: "/react/hooks/useConnectors" },
+            { text: "useContractRead", link: "/react/hooks/useContractRead" },
+            { text: "useContractReads", link: "/react/hooks/useContractReads" },
+            { text: "useContractWrite", link: "/react/hooks/useContractWrite" },
+            { text: "useDisconnect", link: "/react/hooks/useDisconnect" },
+            { text: "useReconnect", link: "/react/hooks/useReconnect" },
+            { text: "useSignMessage", link: "/react/hooks/useSignMessage" },
+            { text: "useSignTypedData", link: "/react/hooks/useSignTypedData" },
+            { text: "useSwitchAccount", link: "/react/hooks/useSwitchAccount" },
+            { text: "useSwitchChain", link: "/react/hooks/useSwitchChain" },
+            { text: "useVerifyMessage", link: "/react/hooks/useVerifyMessage" },
+            { text: "useWalletClient", link: "/react/hooks/useWalletClient" },
+          ],
+        },
+      ],
+    },
+    // {
+    //   text: "Connect",
+    //   items: [],
+    // },
   ],
 });
