f16cc2d17595bacdb2250c9b66b37dbabde76666
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-02-06 14:34:10 +0100

    metamask connector added

--- packages/starkweb/src/exports/connectors.ts
+++ packages/starkweb/src/exports/connectors.ts
@@ -1,5 +1,6 @@
 export { argentX } from "../connectors/argent.js";
 export { braavos } from "../connectors/braavos.js";
 export { injected } from "../connectors/injected.js";
+export { keplr } from "../connectors/keplr.js";
 export { createConnector } from "../core/connectors/createConnector.js";
 
