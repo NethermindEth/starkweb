d98951c98076b462fc38860f1d051ec20a2894be
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-02-08 23:14:30 +0100

    verify message

--- app/next/components/Connect.tsx
+++ app/next/components/Connect.tsx
@@ -1,6 +1,18 @@
 'use client'
 
-import { useChainId, useConnect } from 'starkweb/react'
+import { useState } from 'react';
+
+import {
+  createPublicClient,
+  http,
+  verifyMessage,
+} from 'starkweb';
+import { sepolia } from 'starkweb/chains';
+import {
+  useChainId,
+  useConnect,
+  useSignMessage,
+} from 'starkweb/react';
 
 export function Connect() {
   const chainId = useChainId()
@@ -20,6 +32,63 @@ export function Connect() {
       ))}
       <div>{status}</div>
       <div>{error?.message}</div>
+      <SignMessage />
     </div>
   )
 } 
+
+export function SignMessage() {
+  const { signMessageAsync } = useSignMessage()
+  const [message, setMessage] = useState('')
+  const [signature, setSignature] = useState<string[]>([])
+  const handleSignMessage = async () => { 
+    const sig = await signMessageAsync({ 
+      statement: message,
+      chainId: '0x534e5f4d41494e',
+      domain: 'localhost:3000',
+      uri: 'http://localhost:3000',
+      nonce: Math.random().toString(36).slice(2),
+      version: '1',
+      address: '0x0' // This should be replaced with actual connected wallet address
+    })
+    setSignature(sig.toString())
+  }
+  return <div>
+    <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+    <button onClick={handleSignMessage}>Sign Message</button>
+    <div>{signature}</div>
+    <table>
+      <tr>
+        <td>Message</td>
+        <td>{message}</td>
+      </tr>
+    </table>
+    <div>{signature}</div>
+    <table>
+      <tr>
+        <td>Message</td>
+        <td>{message}</td>
+      </tr>
+    </table>
+    <VerifyMessage message={message} signature={signature} />
+  </div>
+
+}
+
+export function VerifyMessage({message, signature }: { message: string, signature: string[] }) {
+  const [isValid, setIsValid] = useState(false)
+  const handleVerifyMessage = async () => {
+    const isValid = await verifyMessage(
+      createPublicClient({
+      chain: sepolia,
+      transport: http()
+    }), {
+      statement: message 
+    })
+    setIsValid(isValid)
+  }
+  return <div>
+    <button onClick={handleVerifyMessage}>Verify Message</button>
+    <div>{isValid}</div>
+  </div>
+}
