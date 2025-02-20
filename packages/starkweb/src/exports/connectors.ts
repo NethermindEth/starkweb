fe483bf2eb2c55fd2d83a916c33bbb38ba0ccdac
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-02-20 17:31:58 +0100

    removed catridge

--- packages/starkweb/src/exports/connectors.ts
+++ packages/starkweb/src/exports/connectors.ts
@@ -4,4 +4,3 @@ export { injected } from "../connectors/injected.js";
 export { keplr } from "../connectors/keplr.js";
 export { metamask } from "../connectors/metamask.js";
 export { createConnector } from "../core/connectors/createConnector.js";
-export { catridge } from "../connectors/catridge.js";
