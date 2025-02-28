8517cd7793d7d5094f53049ae829ffe0b41b9ba0
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-02-28 02:24:41 +0100

    start of multicalll

--- app/exp001/goodness.ts
+++ app/exp001/goodness.ts
@@ -1,6 +1,7 @@
 import { createPublicClient } from '../../packages/starkweb/dist/esm/clients/createPublicClient'
 import { http } from '../../packages/starkweb/dist/esm/clients/transports/http'
 import { mainnet } from '../../packages/starkweb/dist/esm/chains/definitions/mainnet'
+import { sepolia } from '../../packages/starkweb/dist/esm/chains/definitions/sepolia';
 
   
 export const crimeAbi = [
@@ -796,14 +797,33 @@ export const crimeAbi = [
 
 const client = createPublicClient({
     transport: http(),
-    chain: mainnet,
+    chain: sepolia,
   })
   
-const result = await client.readContract({
-    address: '0x020bd5ec01c672e69e3ca74df376620a6be8a2b104ab70a9f0885be00dd38fb9',
-    abi: crimeAbi,
-    functionName: 'owner',
-    args: [],
+
+  const result = await client.transport.request({
+    jsonrpc: '2.0',
+    method: 'starknet_call',
+    params: [
+      {
+        "calldata":["0x03b56013adf5ce1febefc5dbea283d4a5fb0ce2b9d9e8a174b8e8df005e88fe2"],
+"contract_address":"0x01f7e900d8d0eb877e48c7637496f7727efaccd221c9006ee26f2b2376c9773f","entry_point_selector":"0x00cf37a862e5bf34bd0e858865ea02d4ba6db9cc722f3424eb452c94d4ea567f"
+
+},
+      {
+        "calldata":["0x03b56013adf5ce1febefc5dbea283d4a5fb0ce2b9d9e8a174b8e8df005e88fe2"],
+"contract_address":"0x01f7e900d8d0eb877e48c7637496f7727efaccd221c9006ee26f2b2376c9773f","entry_point_selector":"0x00cf37a862e5bf34bd0e858865ea02d4ba6db9cc722f3424eb452c94d4ea567f"
+
+},
+      "latest"
+],
+    id: 1,
   })
+// const result = await client.readContract({
+//     address: '0x020bd5ec01c672e69e3ca74df376620a6be8a2b104ab70a9f0885be00dd38fb9',
+//     abi: crimeAbi,
+//     functionName: 'owner',
+//     args: [],
+//   })
 
   console.log(result)
